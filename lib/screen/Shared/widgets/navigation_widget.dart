ff520c926c522f742fc77fc75bdb92b9c83f4453
MonkeyCode2006 <24127551@student.hcmus.edu.vn>
2026-01-06 18:15:36 +0700

    Update

--- lib/screen/Shared/widgets/navigation_widget.dart
+++ lib/screen/Shared/widgets/navigation_widget.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
 import 'package:travel_app/screen/Account/screens/account_screen.dart';
 import 'package:travel_app/screen/Favorite/screens/favourite_screen.dart';
-import 'package:travel_app/screen/Home/screens/home_screens.dart';
+import 'package:travel_app/screen/Home/screens/home_screen.dart';
 import 'package:travel_app/screen/Notification/screens/notification_screen.dart';
 import 'package:travel_app/screen/Plan/screens/plan_screen.dart';
 
 class BottomNavigation extends StatelessWidget {
   const BottomNavigation({super.key});
+
+  ItemConfig _buildItem({required IconData icon, required String title}) {
+    return ItemConfig(
+      icon: Icon(icon),
+      title: title,
+      activeForegroundColor: Color(0xFFFFAD35),
+      textStyle: GoogleFonts.beVietnamPro(fontSize: 14),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,42 +26,25 @@ class BottomNavigation extends StatelessWidget {
       tabs: [
         PersistentTabConfig(
           screen: const PlanScreen(),
-          item: ItemConfig(
-            icon: Icon(Icons.map),
-            title: "Plan",
-            activeForegroundColor: Color(0xFFFFAD35),
-          ),
+          item: _buildItem(icon: Icons.map, title: 'Plan'),
         ),
         PersistentTabConfig(
           screen: const FavouriteScreen(),
-          item: ItemConfig(
-            icon: Icon(Icons.favorite),
-            title: 'Favourite',
-            activeForegroundColor: Color(0xFFFFAD35),
-          ),
+          item: _buildItem(icon: Icons.favorite, title: 'Favourite'),
         ),
         PersistentTabConfig(
           screen: const HomeScreens(),
-          item: ItemConfig(
-            icon: Icon(Icons.house),
-            title: "Home",
-            activeForegroundColor: Color(0xFFFFAD35),
-          ),
+          item: _buildItem(icon: Icons.house, title: 'Home'),
         ),
         PersistentTabConfig(
           screen: const NotificationScreen(),
-          item: ItemConfig(
-            icon: Icon(Icons.notifications),
-            title: "Notification",
-            activeForegroundColor: Color(0xFFFFAD35),
-          ),
+          item: _buildItem(icon: Icons.notifications, title: 'Notification'),
         ),
         PersistentTabConfig(
           screen: const AccountScreen(),
-          item: ItemConfig(
-            icon: Icon(Icons.account_circle_rounded),
+          item: _buildItem(
+            icon: Icons.account_circle_rounded,
             title: 'Account',
-            activeForegroundColor: Color(0xFFFFAD35),
           ),
         ),
       ],
